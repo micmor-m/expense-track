18a2164f17c6c63123531168e73f22e98941451a
mic <micmor.m@gmail.com>
2021-06-21 10:53:32 -0400

    first commit

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ExpenseItem () {
+  return(
+    <div>
+      <div>June 1st 2021</div>
+      <div>
+        <h2>Rent</h2>
+        <div>500$</div>
+      </div>
+    </div>
+  )
+}
+
+export default ExpenseItem;
