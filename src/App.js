159e850739e40cb310bea23707c5956a248753f4
mic <micmor.m@gmail.com>
2021-06-23 00:13:13 -0400

    update html to separate day, year and month

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expenses = [
-    { id: "e1", title: "Car Insurance", amount: 300, date: "June 21, 2021" },
-    { id: "e2", title: "Grocery", amount: 56.67, date: "June 20, 2021" },
-    { id: "e3", title: "Rent", amount: 1200, date: "June 1, 2021" },
-    { id: "e4", title: "Book", amount: 26, date: "June 15, 2021" },
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 300,
+      date: new Date("June 21, 2021"),
+    },
+    {
+      id: "e2",
+      title: "Grocery",
+      amount: 56.67,
+      date: new Date("June 20, 2021"),
+    },
+    { id: "e3", title: "Rent", amount: 1200, date: new Date("June 1, 2021") },
+    { id: "e4", title: "Book", amount: 26, date: new Date("June 15, 2021") },
   ];
 
   return (
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -3,9 +3,17 @@ import "./ExpenseItem.css"
 
 function ExpenseItem ({title, amount, date}) {
 
+  const month = date.toLocaleString("en-US", {month: "long"})
+  const year = date.getFullYear();
+  const day = date.toLocaleString("en-US", {day: "2-digit"})
+
   return(
     <div className="expense-item">
-      <div>{date}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{amount}$</div>
