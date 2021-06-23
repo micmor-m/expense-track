bfc5dc15ee1d17ed9b26fb1de2ca3c5e45c7e391
mic <micmor.m@gmail.com>
2021-06-23 00:30:34 -0400

    move date in a separate component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,19 +1,12 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"
 
 function ExpenseItem ({title, amount, date}) {
 
-  const month = date.toLocaleString("en-US", {month: "long"})
-  const year = date.getFullYear();
-  const day = date.toLocaleString("en-US", {day: "2-digit"})
-
   return(
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+     <ExpenseDate date={date}/>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{amount}$</div>
