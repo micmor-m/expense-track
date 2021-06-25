549e7e8624fc1f3a1771a33bcdb200c800d47cd6
mic <micmor.m@gmail.com>
2021-06-24 23:38:01 -0400

    add useState for title

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css"
 
-function ExpenseItem ({title, amount, date}) {
+function ExpenseItem (props) {
+
+  const [title, setTitle] = useState(props.title)
+
+  const clickHandler = () => {
+    setTitle("My new title")
+  }
 
   return(
     <Card className="expense-item">
-     <ExpenseDate date={date}/>
+     <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">{amount}$</div>
+        <div className="expense-item__price">{props.amount}$</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
