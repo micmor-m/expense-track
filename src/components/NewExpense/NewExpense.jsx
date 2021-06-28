21c753e059cfe2f83af3dbbdfd9459d4178b3ed7
mic <micmor.m@gmail.com>
2021-06-28 01:23:32 -0400

    add form style

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import "./NewExpense.css"
+
+
 const NewExpense = (props) => {
 
 const [date, setDate] = useState("")
@@ -21,15 +24,29 @@ function amountHandler(event){
 const expense = {date, title, amount}
 
   return (
-    <form action="" method="">
-      <label >Date</label>
-      <input id="date" onChange={dateHandler} type="date" min="2021-01-01" max="2022-12-31" value={date} placeholder="January 01, 2021"/>
-      <label >Title</label>
-      <input id="title" onChange={titleHandler} type="text" value={title} placeholder="My expense"/>
-      <label >Amount</label>
-      <input id="amount" onChange={amountHandler} type="number" min="0.01" step="0.01" value={amount} placeholder="100"/>
-      <button onClick={(event)=> {event.preventDefault(); props.expenseHandler(expense) }}>Add Expense</button>
-    </form>
+    <div className="new-expense">
+      <form action="" method="">
+        <div className="new-expense__controls">
+          <div  className="new-expense__control">
+            <label >Title</label>
+            <input id="title" onChange={titleHandler} type="text" value={title} placeholder="My expense"/>
+          </div>
+          <div className="new-expense__control">
+            <label >Amount</label>
+            <input id="amount" onChange={amountHandler} type="number" min="0.01" step="0.01" value={amount} placeholder="100"/>
+          </div>
+          <div className="new-expense__control">
+            <label >Date</label>
+            <input id="date" onChange={dateHandler} type="date" min="2021-01-01" max="2022-12-31" value={date} placeholder="January 01, 2021"/>
+          </div>
+         
+        </div>
+        <div className="new-expense__actions">
+        <button onClick={(event)=> {event.preventDefault(); props.expenseHandler(expense) }}>Add Expense</button>
+        </div>
+      </form>
+    </div>
+   
   )
 }
 
